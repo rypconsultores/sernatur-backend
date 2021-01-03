5c5e74f3a0ca38bfebef1bf639d33d6ad1bf1ef4
Felipe Buccioni <falcacibar@gmail.com>
2021-01-03 03:26:47 -0300

    Fix: Update place problems

--- apps/c19trace/serializers/place.py
+++ apps/c19trace/serializers/place.py
@@ -16,6 +16,31 @@ class PlaceInput(serializers.ModelSerializer):
         label=models.PlaceCheckPoint._meta.verbose_name_plural, many=True
     )
 
+    def update(self, validated_data):
+        foreign_key_nesteds = ('check_points',)
+        list_values = {}
+
+        with transaction.atomic():
+            for field in foreign_key_nesteds:
+                if field in validated_data:
+                    list_values[field] = validated_data.pop(field)
+
+            instance = super().update(validated_data)
+
+            for field in foreign_key_nesteds:
+                field_serializer = self.fields[field]
+                getattr(self.instance, 'field').delete()
+
+                if field in list_values:
+                    for value_item in list_values[field]:
+                        value_item_serialized: serializers.ModelSerializer = \
+                            field_serializer.child.__class__(data=value_item)
+                        value_item_serialized.is_valid(raise_exception=True)
+                        value_item_serialized._validated_data['place_id'] = instance.id
+                        value_item_serialized.save()
+
+        return instance
+
     def create(self, validated_data):
         foreign_key_nesteds = ('check_points',)
         list_values = {}
